a53bdd1eee0af2c469404022b400cddbd4c5fb98
akmitrich <ak.mitrich@mail.ru>
2023-02-28 19:58:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+# This is a simple Python script.
 def lattice(cells, basis, result, current, n):
     current.append(0.0)
     for i in range(cells[n]):
